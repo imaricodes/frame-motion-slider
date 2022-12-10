c494e9744b4f6e8a14fe774a9c77ef8879972ddd
Imari Childress <imaricodes@gmail.com>
2022-12-10 09:27:33 -0500

    Push to remote

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import { motion } from "framer-motion";
+import { useRef, useState, useEffect } from "react";
+import images from './images'
 
 function App() {
+
+const [width, setWidth] = useState(0);
+const carousel = useRef()
+
+useEffect( ()=>{
+console.log(carousel.currenti)
+  
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div ref={carousel} className="carousel">
+        <motion.div drag="x" dragConstraints={{right: 0, left: 0}} className="inner-carousel">
+          {images.map( (image, index) => {
+            let id = index
+            console.log(`id ${id}`)
+            return (
+              <motion.div key={`img${index}`} className="item">
+                <img src={image} alt=''/>
+              </motion.div>
+            )
+          })}
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
+
+<style>
+
+</style>
 
 export default App;
